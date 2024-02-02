10ecf0a297af9d16d0c60dab07fe3370339bcc22
DJdaGOAT1 <149193211+DJdaGOAT1@users.noreply.github.com>
2024-02-02 11:54:26 -0600

    Set up files with methods and constructors

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -2,11 +2,41 @@
 //
 // You should delete this comment and replace it with your class
 // header comment.
+import java.io.PrintStream;
+import java.util.*;
 
 public class QuestionsGame {
     // Your code here
 
     private static class QuestionNode {
-        // Your code here
+        public final String data;
+        public QuestionNode left;
+        public QuestionNode right;
+        
+        public QuestionNode(String data, QuestionNode left, QuestionNode right) {
+        	this.data = data;
+        	this.left = left;
+        	this.right = right;
+        }
     }
+    
+    public QuestionsGame(String object) {
+    	QuestionsGame obj = new QuestionsGame(object);
+    }
+    
+    public QuestionsGame(Scanner input) {
+    	QuestionsGame scan = new QuestionsGame(input);
+    }
+    
+    public void saveQuestions(PrintStream output) {
+    	if(output == null) {
+    		throw new IllegalArgumentException();
+    	}
+    }
+    
+    public void play() {
+    	
+    }
+    
+    
 }
